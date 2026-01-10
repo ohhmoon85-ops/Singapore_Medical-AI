babc78051997f107a79c5669446475f1ad77c207
ohhmoon85-ops <ohhmoon85@gmail.com>
2026-01-10 22:49:14 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App'; // 확장자 .js는 생략해도 됩니다.
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 
-const rootElement = document.getElementById('root');
-const root = ReactDOM.createRoot(rootElement);
-root.render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
